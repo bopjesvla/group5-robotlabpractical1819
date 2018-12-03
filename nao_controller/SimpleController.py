cb1e6f685e3d68bada44f71f2ecdbae915f54d15
s1014012 <sameera.bogodaarachchige@student.ru.nl>
2018-12-03 13:33:40 +0100

    w5 pipeline ready

--- nao_controller/SimpleController.py
+++ nao_controller/SimpleController.py
@@ -411,6 +411,32 @@ class SimpleController:
 
         def w5ComeWithMe():
             print 'w5ComeWithMe'
+            # brigit - Come with me 1, 3, 5, 7
+
+            # 1. Eyes RED
+
+            # 2. Terminator vision turns on, specications like last time, 
+            # so with bounding boxes around the face, etc.
+            
+            # 3. Nao walks toward the human actor sitting on the floor. 
+            # Nao stops close to the actor. Make sure the Nao doesn't bump into the actor.
+
+            # 4. When a face is located, the Text on the terminator vision should be HELP.
+
+            # 5. Nao goes into a crouching position, facing the human, see Figure 1.
+
+            # 6. At the same time the Nao says COME WITH ME IF YOU WANT TO LIVE. = Sameera
+            # See the python files in the Gitlab repo for examples of running parallel processes on the Nao.
+
+            # 7. The Nao goes back into standing position and walks 2 meters in a random direction.
+
+            # 8. The Nao turns around and looks at the actor.
+
+            # 9. The Nao does a 'come here' gesture with its arm, while saying COME HERE SARAH CONNOR, NAO!.
+
+            # 10. The actor stands up and walks towards the Nao
+
+
 
         # hasta la vista
         Button(frame,
@@ -421,6 +447,29 @@ class SimpleController:
 
         def w5HastaLaVista():
             print 'w5HastaLaVista'
+            # brigit - Hasta la vista 1,2
+
+            # 1. While the actor is walking towards the Nao, the Nao sits down.
+
+            # 2. The Nao eye LEDs turn green.
+
+            # 3. The terminator vision gets a green overlay.
+
+            # 4. The actor sits down facing the Nao.
+
+            # 5. The actor touches the Nao's head sensor, the Nao says AFFIRMATIVE, enters parrot mode.
+
+            # 6. Actor says NO PROBLEMO, Nao repeats.
+
+            # 7. Actor says BITE ME, Nao repeats.
+
+            # 8. Actor says HASTA LA VISTA BABY, Nao repeats.
+
+            # 9. Actor touches Nao's head, Nao exits parrot mode and says TALK TO THE HAND.
+
+            # 10. When the director Nao hears this, it says CUT!.
+
+            # 11. The Nao stands up and walks to the middle of the room.
 
 
     def makeXEntry(self):
